9c2372a30a458ebb9da5b3930205e2002b305878
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-01-10 13:59:12 -0500

    Add computer choice and human choice

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Test")
+function getComputerChoice() {
+    // make an array with all the options
+    let options = ["Rock", "Paper", "Scissors"];
+    // get a random array element
+    let randomElement = Math.floor(Math.random() * options.length);
+    // return the random element
+    return options[randomElement];
+}
+
+function getHumanChoice() {
+    let isCorrect = false;
+    // while isCorrect === false
+    while (isCorrect === false) {
+        // get user input
+        let userInput = prompt("Rock Paper or Scissors?");
+
+        // make user input lowercase
+        userInput = userInput.toLocaleLowerCase();
+
+        // if userinput === rock paper or scissors
+        if (userInput === "rock" || userInput === "paper" || userInput === "scissors"){
+            // isCorrect = true
+            isCorrect = true;
+            // return userInput
+            return userInput
+        }
+    }
+    
+}
+
+console.log(getHumanChoice())
