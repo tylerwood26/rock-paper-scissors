484e46ca2e1069fe07337008e584938aaf266bbe
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-01-11 17:14:42 -0500

    Add playRound()

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     // make an array with all the options
-    let options = ["Rock", "Paper", "Scissors"];
+    let options = ["rock", "paper", "scissors"];
     // get a random array element
     let randomElement = Math.floor(Math.random() * options.length);
     // return the random element
@@ -18,7 +18,7 @@ function getHumanChoice() {
         userInput = userInput.toLocaleLowerCase();
 
         // if userinput === rock paper or scissors
-        if (userInput === "rock" || userInput === "paper" || userInput === "scissors"){
+        if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
             // isCorrect = true
             isCorrect = true;
             // return userInput
@@ -31,4 +31,24 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+    // if its a tie
+    if (humanChoice === computerChoice) {
+        // console.log its a tie
+        return console.log(`It's a tie! Both chose ${humanChoice}!`);
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") || 
+    (humanChoice === "paper" && computerChoice === "rock") || 
+    (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore = humanScore + 1;
+        return console.log(`You won! ${humanChoice} beats ${computerChoice}!`);
+    } else {
+        computerScore = computerScore + 1;
+        return console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+    }
+    // return console.log of winner
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
